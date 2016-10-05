db0816b7b75fbeddb5b062b21cf6eec1951fbc41
joeyrobert <joey.robert@rangle.io>
2016-10-05 12:56:29 -0400

    Introduce SystemJS instead of script tags

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,10 @@
+require('node_modules/angular/angular.js');
+require('node_modules/angular-route/angular-route.js');
+require('node_modules/angular-resource/angular-resource.js');
+require('./app.js');
+require('./controllers/todoCtrl.js');
+require('./services/todoStorage.js');
+require('./directives/todoFocus.js');
+require('./directives/todoEscape.js');
+
 angular.bootstrap(document.body, ['todomvc']);
