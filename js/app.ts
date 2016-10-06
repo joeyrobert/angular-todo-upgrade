dcd6442c4d47d9513e20b0cfe983e395c3455294
joeyrobert <joey.robert@rangle.io>
2016-10-06 15:16:01 -0400

    App template in file

--- js/app.ts
+++ js/app.ts
@@ -11,7 +11,7 @@ angular.module('todomvc', ['ngRoute', 'ngResource'])
 
     var routeConfig = {
       controller: 'TodoCtrl',
-      templateUrl: 'todomvc-index.html',
+      templateUrl: '/js/app.html',
       resolve: {
         store: function (todoStorage) {
           // Get the correct module (API or localStorage).
