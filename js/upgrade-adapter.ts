16c77965b31db51acd34ab803045c07963d0ea12
joeyrobert <joey.robert@rangle.io>
2016-10-07 10:53:22 -0400

    Enable prod mode

--- js/upgrade-adapter.ts
+++ js/upgrade-adapter.ts
@@ -1,5 +1,7 @@
 // Angular 2
 import {UpgradeAdapter} from '@angular/upgrade';
 import {AppModule} from './app.module';
+import {enableProdMode} from '@angular/core';
 
 export const upgradeAdapter = new UpgradeAdapter(AppModule);
+enableProdMode();
